3bc5ea967d38838088e500639bc7353e999d1dd6
Bami <bbamii@outlook.com>
2021-08-20 06:58:52 +0100

    working

---
+++ js/dashboard.js
@@ -0,0 +1,21 @@
+(function(){
+  const dashboardNavTag =  document.querySelectorAll('[data-nav-tag]')
+  let prev;
+
+  Array.from(dashboardNavTag).forEach(tag => {
+    tag.onclick = function({ target }) {
+      const tagTarget = target.dataset.navTag;
+      if (prev == tagTarget) {
+        document.querySelector(`[data-nav-tag-target=${tagTarget}`).classList.remove('expanded')
+        return
+      }
+      if (prev) document.querySelector(`[data-nav-tag-target=${prev}`).classList.remove('expanded')
+      document.querySelector(`[data-nav-tag-target=${tagTarget}`).classList.add('expanded')
+      prev = tagTarget
+    }
+  })
+
+  document.getElementById('notification-close').onclick = function () {
+    document.getElementById('notification').classList.remove('active')
+  }
+})()
