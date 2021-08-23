02fa8c952863ccf3b95e4d6c4e44338ef4ba0d01
Bami <bbamii@outlook.com>
2021-08-23 11:38:08 +0100

    repaymentdsfa

--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,16 @@
 (function() {
-  let currTab = document.querySelector('[data-tab-target].selected').dataset.tabTarget;
   const tabButtons = document.querySelectorAll('[data-tab]');
 
   Array.from(tabButtons).forEach(tab => {
     const idx = tab.dataset.tab;
+    const tabGroup = tab.dataset.tabGroup;
     
     tab.onclick = function() {
-      document.querySelector(`[data-tab-target="${currTab}"]`).classList.remove('selected');
-      document.querySelector(`[data-tab-target="${idx}"]`).classList.add('selected');
+      document.querySelector(`[data-tab-group=${tabGroup}][data-tab-target].selected`).classList.remove('selected');
+      document.querySelector(`[data-tab-group=${tabGroup}][data-tab-target="${idx}"]`).classList.add('selected');
       
-      document.querySelector(`[data-tab="${currTab}"]`).classList.remove('selected');
-      document.querySelector(`[data-tab="${idx}"]`).classList.add('selected');
+      document.querySelector(`[data-tab-group=${tabGroup}][data-tab].selected`).classList.remove('selected');
+      document.querySelector(`[data-tab-group=${tabGroup}][data-tab="${idx}"]`).classList.add('selected');
       currTab = idx;
     }
   })
