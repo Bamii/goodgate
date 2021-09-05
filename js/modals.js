2493b2a1d2d43f02b15c6bbb02232c9ac1926b91
Bami <bbamii@outlook.com>
2021-09-05 15:50:51 +0100

    investments

--- js/modals.js
+++ js/modals.js
@@ -1,10 +1,19 @@
 (function() {
   const dataModalButton = document.querySelectorAll('[data-modal]')
+  let currModal = null;
   
   Array.from(dataModalButton).forEach(btn => {
-    const modalTarget = btn.dataset.modal;
+    
+    btn.onclick = function ({ target }) {
+      const modalTarget = target.dataset.modal;
+      
+      if(document.querySelector(`.modal.visible`))
+        document.querySelector(`.modal.visible`).classList.remove('visible')
 
-    btn.onclick = function () {
+      if(currModal === modalTarget) {
+        currModal = null
+        return
+      };
       currModal = modalTarget
       document.querySelector(`[data-modal-target=${modalTarget}]`).classList.toggle('visible')
     }
